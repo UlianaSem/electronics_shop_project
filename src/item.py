553f5c3e12170c36a5bfe8cd4132e110cf997f85
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-03 12:47:48 +0500

    Update item class - add getter and setter for name - add instantiate_from_csv classmethod - add string_to_number staticmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) < 11:
+            self.__name = new_name
+        else:
+            raise Exception('Длина наименования товара превышает 10 символов.')
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +45,23 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Метод,инициализирующий экземпляры класса Item данными из файла src/items.csv"""
+        cls.all.clear()
+
+        with open('../src/items.csv', 'r', encoding='Windows-1251') as csv_file:
+            reader = csv.DictReader(csv_file)
+
+            for line in reader:
+                cls(line['name'], float(line['price']), int(line['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        """
+        Возвращает число из числа-строки.
+        :param string: число в str
+        :return: число в int
+        """
+        return int(float(string))

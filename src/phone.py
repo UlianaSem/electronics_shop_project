fffc1e86b3a6ccaad1e9a254babc823207527b46
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-12 20:15:18 +0500

    Create phone.py

---
+++ src/phone.py
@@ -0,0 +1,21 @@
+import src.item
+
+
+class Phone(src.item.Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, sim_number):
+        if isinstance(sim_number, int) and sim_number > 0:
+            self.__number_of_sim = sim_number
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')

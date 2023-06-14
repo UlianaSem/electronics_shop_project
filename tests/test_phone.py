d2be668ce6a754ddac0011cefeb329694e0e1baa
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-14 16:04:08 +0500

    Move test_add finction from test_item.py to test_phone.py

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,5 +1,6 @@
 import pytest
 import src.phone
+import src.item
 
 
 @pytest.fixture
@@ -20,3 +21,12 @@ def test_number_of_sim(get_phone_for_test):
 
     with pytest.raises(ValueError):
         get_phone_for_test.number_of_sim = 0
+
+
+def test_add():
+    item1 = src.item.Item("Смартфон", 10000, 20)
+    phone1 = src.phone.Phone("iPhone 14", 120_000, 5, 2)
+
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+    assert phone1 + item1 == 25

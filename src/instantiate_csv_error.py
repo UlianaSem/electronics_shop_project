14866b6534184263ab2d24a8287539419838afbc
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-19 18:16:17 +0500

    Update instantiate_csv_error.py > add docstrings

--- src/instantiate_csv_error.py
+++ src/instantiate_csv_error.py
@@ -1,4 +1,5 @@
 class InstantiateCSVError(Exception):
+    """Класс-исключение для проверки колонок в файле items.csv"""
 
     def __init__(self, *args, **kwargs):
         self.message = args[0] if args else 'Файл item.csv поврежден'
